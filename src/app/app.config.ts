1353b4f82c43ffcd6e4f965798355b3cc9b50cc9
nkurjluc <nkurjluc@gmail.com>
2025-03-20 15:51:41 -0400

    Dernier commit du 20/03/2025

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,8 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 
+
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
