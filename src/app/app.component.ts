0acd3017d872ef8089cd0360d405a50a48430aba
nkurjluc <nkurjluc@gmail.com>
2025-03-20 14:24:32 -0400

    Commit du 20/03/2025

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./components/login/login.component";
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [LoginComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'facturationFontEnd';
+  title = 'facturationFrontEnd';
 }
